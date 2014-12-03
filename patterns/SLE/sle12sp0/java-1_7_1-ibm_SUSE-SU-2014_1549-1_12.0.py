bb8ff3ef2b35846d1d52f661523c940ceac156c7
Jason Record <g23guy@gmail.com>
2014-12-03 15:02:15 -0700

    new pattern

---
+++ patterns/SLE/sle12sp0/java-1_7_1-ibm_SUSE-SU-2014_1549-1_12.0.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+#
+# Title:       Important Security Announcement for java-1_7_1-ibm SUSE-SU-2014:1549-1
+# Description: Security fixes for SUSE Linux Enterprise 12 SP0
+# Source:      Security Announcement Parser v1.0.10
+# Modified:    2014 Dec 03
+#
+##############################################################################
+# Copyright (C) 2014 SUSE LLC
+##############################################################################
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+#  Authors/Contributors:
+#   Jason Record (jrecord@suse.com)
+#
+##############################################################################
+
+import os
+import Core
+import SUSE
+
+META_CLASS = "Security"
+META_CATEGORY = "SLE"
+META_COMPONENT = "java-1_7_1-ibm"
+PATTERN_ID = os.path.basename(__file__)
+PRIMARY_LINK = "META_LINK_Security"
+OVERALL = Core.TEMP
+OVERALL_INFO = "NOT SET"
+OTHER_LINKS = "META_LINK_Security=http://lists.opensuse.org/opensuse-security-announce/2014-12/msg00002.html"
+Core.init(META_CLASS, META_CATEGORY, META_COMPONENT, PATTERN_ID, PRIMARY_LINK, OVERALL, OVERALL_INFO, OTHER_LINKS)
+
+LTSS = False
+NAME = 'java-1_7_1-ibm'
+MAIN = ''
+SEVERITY = 'Important'
+TAG = 'SUSE-SU-2014:1549-1'
+PACKAGES = {}
+SERVER = SUSE.getHostInfo()
+
+if ( SERVER['DistroVersion'] == 12):
+	if ( SERVER['DistroPatchLevel'] == 0 ):
+		PACKAGES = {
+			'java-1_7_1-ibm-jdbc': '1.7.1_sr2.0-4.1',
+			'java-1_7_1-ibm-devel': '1.7.1_sr2.0-4.1',
+			'java-1_7_1-ibm-plugin': '1.7.1_sr2.0-4.1',
+			'java-1_7_1-ibm-alsa': '1.7.1_sr2.0-4.1',
+			'java-1_7_1-ibm': '1.7.1_sr2.0-4.1',
+		}
+		SUSE.securityAnnouncementPackageCheck(NAME, MAIN, LTSS, SEVERITY, TAG, PACKAGES)
+	else:
+		Core.updateStatus(Core.ERROR, "ERROR: " + NAME + " Security Announcement: Outside the service pack scope")
+else:
+	Core.updateStatus(Core.ERROR, "ERROR: " + NAME + " Security Announcement: Outside the distribution scope")
+Core.printPatternResults()
+
