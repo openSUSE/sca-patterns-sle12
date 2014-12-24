f2d015d7d541b7725173a43ca874f1a96d25700d
Jason Record <g23guy@gmail.com>
2014-12-24 12:44:58 -0700

    new patterns

---
+++ patterns/SLE/sle12sp0/ntp_SUSE-SU-2014_1690-1_12.0.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+#
+# Title:       Critical Security Announcement for ntp SUSE-SU-2014:1690-1
+# Description: Security fixes for SUSE Linux Enterprise 12 SP0
+# Source:      Security Announcement Parser v1.1.8
+# Modified:    2014 Dec 24
+#
+##############################################################################
+# Copyright (C) 2014 SUSE LLC
+##############################################################################
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+#  Authors/Contributors:
+#   Jason Record (jrecord@suse.com)
+#
+##############################################################################
+
+import os
+import Core
+import SUSE
+
+META_CLASS = "Security"
+META_CATEGORY = "SLE"
+META_COMPONENT = "ntp"
+PATTERN_ID = os.path.basename(__file__)
+PRIMARY_LINK = "META_LINK_Security"
+OVERALL = Core.TEMP
+OVERALL_INFO = "NOT SET"
+OTHER_LINKS = "META_LINK_Security=http://lists.opensuse.org/opensuse-security-announce/2014-12/msg00026.html"
+Core.init(META_CLASS, META_CATEGORY, META_COMPONENT, PATTERN_ID, PRIMARY_LINK, OVERALL, OVERALL_INFO, OTHER_LINKS)
+
+LTSS = False
+NAME = 'ntp'
+MAIN = ''
+SEVERITY = 'Critical'
+TAG = 'SUSE-SU-2014:1690-1'
+PACKAGES = {}
+SERVER = SUSE.getHostInfo()
+
+if ( SERVER['DistroVersion'] == 12):
+	if ( SERVER['DistroPatchLevel'] == 0 ):
+		PACKAGES = {
+			'ntp': '4.2.6p5-31.1',
+			'ntp-debuginfo': '4.2.6p5-31.1',
+			'ntp-debugsource': '4.2.6p5-31.1',
+			'ntp-doc': '4.2.6p5-31.1',
+		}
+		SUSE.securityAnnouncementPackageCheck(NAME, MAIN, LTSS, SEVERITY, TAG, PACKAGES)
+	else:
+		Core.updateStatus(Core.ERROR, "ERROR: " + NAME + " Security Announcement: Outside the service pack scope")
+else:
+	Core.updateStatus(Core.ERROR, "ERROR: " + NAME + " Security Announcement: Outside the distribution scope")
+Core.printPatternResults()
+
