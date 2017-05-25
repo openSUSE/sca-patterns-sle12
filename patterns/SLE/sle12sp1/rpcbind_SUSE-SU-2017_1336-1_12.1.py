f0b9491e0d5c1478d4fe47c087652256a5010992
Jason Record <g23guy@gmail.com>
2017-05-25 08:00:19 -0600

    new security patterns

---
+++ patterns/SLE/sle12sp1/rpcbind_SUSE-SU-2017_1336-1_12.1.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+#
+# Title:       Important Security Announcement for rpcbind SUSE-SU-2017:1336-1
+# Description: Security fixes for SUSE Linux Enterprise 12 SP1
+# Source:      Security Announcement Parser v1.3.6
+# Modified:    2017 May 25
+#
+##############################################################################
+# Copyright (C) 2017 SUSE LLC
+##############################################################################
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+#  Authors/Contributors:
+#   Jason Record (jason.record@suse.com)
+#
+##############################################################################
+
+import os
+import Core
+import SUSE
+
+META_CLASS = "Security"
+META_CATEGORY = "SLE"
+META_COMPONENT = "rpcbind"
+PATTERN_ID = os.path.basename(__file__)
+PRIMARY_LINK = "META_LINK_Security"
+OVERALL = Core.TEMP
+OVERALL_INFO = "NOT SET"
+OTHER_LINKS = "META_LINK_Security=http://lists.opensuse.org/opensuse-security-announce/2017-05/msg00056.html"
+Core.init(META_CLASS, META_CATEGORY, META_COMPONENT, PATTERN_ID, PRIMARY_LINK, OVERALL, OVERALL_INFO, OTHER_LINKS)
+
+LTSS = False
+NAME = 'rpcbind'
+MAIN = ''
+SEVERITY = 'Important'
+TAG = 'SUSE-SU-2017:1336-1'
+PACKAGES = {}
+SERVER = SUSE.getHostInfo()
+
+if ( SERVER['DistroVersion'] == 12):
+	if ( SERVER['DistroPatchLevel'] == 1 ):
+		PACKAGES = {
+			'rpcbind': '0.2.1_rc4-17.3.1',
+			'rpcbind-debuginfo': '0.2.1_rc4-17.3.1',
+			'rpcbind-debugsource': '0.2.1_rc4-17.3.1',
+		}
+		SUSE.securityAnnouncementPackageCheck(NAME, MAIN, LTSS, SEVERITY, TAG, PACKAGES)
+	else:
+		Core.updateStatus(Core.ERROR, "ERROR: " + NAME + " Security Announcement: Outside the service pack scope")
+else:
+	Core.updateStatus(Core.ERROR, "ERROR: " + NAME + " Security Announcement: Outside the distribution scope")
+Core.printPatternResults()
+
