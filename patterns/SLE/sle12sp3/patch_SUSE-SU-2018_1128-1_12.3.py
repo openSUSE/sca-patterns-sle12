96725cd14a410bfd469e23f23add08fee63f0cf0
Jason Record <g23guy@gmail.com>
2018-06-11 12:10:31 -0600

    New Security Announcement Patterns

---
+++ patterns/SLE/sle12sp3/patch_SUSE-SU-2018_1128-1_12.3.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+#
+# Title:       Important Security Announcement for patch SUSE-SU-2018:1128-1
+# Description: Security fixes for SUSE Linux Enterprise 12 SP3
+# Source:      Security Announcement Parser v1.3.7
+# Modified:    2018 Jun 11
+#
+##############################################################################
+# Copyright (C) 2018 SUSE LLC
+##############################################################################
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+#  Authors/Contributors:
+#   Jason Record (jason.record@suse.com)
+#
+##############################################################################
+
+import os
+import Core
+import SUSE
+
+META_CLASS = "Security"
+META_CATEGORY = "SLE"
+META_COMPONENT = "patch"
+PATTERN_ID = os.path.basename(__file__)
+PRIMARY_LINK = "META_LINK_Security"
+OVERALL = Core.TEMP
+OVERALL_INFO = "NOT SET"
+OTHER_LINKS = "META_LINK_Security=https://lists.opensuse.org/opensuse-security-announce/2018-05/msg00001.html"
+Core.init(META_CLASS, META_CATEGORY, META_COMPONENT, PATTERN_ID, PRIMARY_LINK, OVERALL, OVERALL_INFO, OTHER_LINKS)
+
+LTSS = False
+NAME = 'patch'
+MAIN = ''
+SEVERITY = 'Important'
+TAG = 'SUSE-SU-2018:1128-1'
+PACKAGES = {}
+SERVER = SUSE.getHostInfo()
+
+if ( SERVER['DistroVersion'] == 12):
+	if ( SERVER['DistroPatchLevel'] == 3 ):
+		PACKAGES = {
+			'patch': '2.7.5-8.5.1',
+			'patch-debuginfo': '2.7.5-8.5.1',
+			'patch-debugsource': '2.7.5-8.5.1',
+		}
+		SUSE.securityAnnouncementPackageCheck(NAME, MAIN, LTSS, SEVERITY, TAG, PACKAGES)
+	else:
+		Core.updateStatus(Core.ERROR, "ERROR: " + NAME + " Security Announcement: Outside the service pack scope")
+else:
+	Core.updateStatus(Core.ERROR, "ERROR: " + NAME + " Security Announcement: Outside the distribution scope")
+Core.printPatternResults()
+
