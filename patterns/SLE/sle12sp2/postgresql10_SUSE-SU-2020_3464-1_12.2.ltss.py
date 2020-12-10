29e3b3efcf1707566d431628664a3047fa565ef0
Jason Record <jason.record@gmail.com>
2020-12-10 15:10:52 -0700

    Security announcement patterns for SUSE-SU-2020:3516-1 SUSE-SU-2020:3354-1 SUSE-SU-2020:3501-1 SUSE-SU-2020:3433-1 SUSE-SU-2020:3379-1 SUSE-SU-2020:3550-1 SUSE-SU-2020:3548-1 SUSE-SU-2020:3464-1 SUSE-SU-2020:3477-1 SUSE-SU-2020:3514-1 SUSE-SU-2020:3367-1 SUSE-SU-2020:3516-1 SUSE-SU-2020:3354-1 SUSE-SU-2020:3503-1 SUSE-SU-2020:3433-1 SUSE-SU-2020:3379-1 SUSE-SU-2020:3550-1 SUSE-SU-2020:3548-1 SUSE-SU-2020:3464-1 SUSE-SU-2020:3477-1 SUSE-SU-2020:3474-1 SUSE-SU-2020:3514-1 SUSE-SU-2020:3516-1 SUSE-SU-2020:3353-1 SUSE-SU-2020:3544-1 SUSE-SU-2020:3379-1 SUSE-SU-2020:3550-1 SUSE-SU-2020:3497-1 SUSE-SU-2020:3548-1 SUSE-SU-2020:3464-1 SUSE-SU-2020:3514-1 SUSE-SU-2020:3415-1 SUSE-SU-2020:3516-1 SUSE-SU-2020:3353-1 SUSE-SU-2020:3379-1 SUSE-SU-2020:3550-1 SUSE-SU-2020:3497-1 SUSE-SU-2020:3548-1 SUSE-SU-2020:3464-1 SUSE-SU-2020:3563-1 SUSE-SU-2020:3360-1 SUSE-SU-2020:3457-1 SUSE-SU-2020:3424-1 SUSE-SU-2020:3414-1

---
+++ patterns/SLE/sle12sp2/postgresql10_SUSE-SU-2020_3464-1_12.2.ltss.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+#
+# Title:       Important Security Announcement for postgresql10 SUSE-SU-2020:3464-1
+# Description: Security fixes for SUSE Linux Enterprise 12 SP2 LTSS
+# Source:      Security Announcement Parser v1.5.2
+# Modified:    2020 Dec 10
+#
+##############################################################################
+# Copyright (C) 2020 SUSE LLC
+##############################################################################
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+#  Authors/Contributors:
+#   Jason Record (jason.record@suse.com)
+#
+##############################################################################
+
+import os
+import Core
+import SUSE
+
+META_CLASS = "Security"
+META_CATEGORY = "SLE"
+META_COMPONENT = "postgresql10"
+PATTERN_ID = os.path.basename(__file__)
+PRIMARY_LINK = "META_LINK_Security"
+OVERALL = Core.TEMP
+OVERALL_INFO = "NOT SET"
+OTHER_LINKS = "META_LINK_Security=https://lists.suse.com/pipermail/sle-security-updates/2020-November/007828.html"
+Core.init(META_CLASS, META_CATEGORY, META_COMPONENT, PATTERN_ID, PRIMARY_LINK, OVERALL, OVERALL_INFO, OTHER_LINKS)
+
+LTSS = True
+NAME = 'postgresql10'
+MAIN = ''
+SEVERITY = 'Important'
+TAG = 'SUSE-SU-2020:3464-1'
+PACKAGES = {}
+SERVER = SUSE.getHostInfo()
+
+if ( SERVER['DistroVersion'] == 12):
+	if ( SERVER['DistroPatchLevel'] == 2 ):
+		PACKAGES = {
+			'postgresql10': '10.15-4.9.1',
+			'postgresql10-contrib': '10.15-4.9.1',
+			'postgresql10-contrib-debuginfo': '10.15-4.9.1',
+			'postgresql10-debuginfo': '10.15-4.9.1',
+			'postgresql10-debugsource': '10.15-4.9.1',
+			'postgresql10-docs': '10.15-4.9.1',
+			'postgresql10-plperl': '10.15-4.9.1',
+			'postgresql10-plperl-debuginfo': '10.15-4.9.1',
+			'postgresql10-plpython': '10.15-4.9.1',
+			'postgresql10-plpython-debuginfo': '10.15-4.9.1',
+			'postgresql10-pltcl': '10.15-4.9.1',
+			'postgresql10-pltcl-debuginfo': '10.15-4.9.1',
+			'postgresql10-server': '10.15-4.9.1',
+			'postgresql10-server-debuginfo': '10.15-4.9.1',
+		}
+		SUSE.securityAnnouncementPackageCheck(NAME, MAIN, LTSS, SEVERITY, TAG, PACKAGES)
+	else:
+		Core.updateStatus(Core.ERROR, "ERROR: " + NAME + " Security Announcement: Outside the service pack scope")
+else:
+	Core.updateStatus(Core.ERROR, "ERROR: " + NAME + " Security Announcement: Outside the distribution scope")
+Core.printPatternResults()
+
---
+++ patterns/SLE/sle12sp5/python36_SUSE-SU-2020_3563-1_12.5.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+#
+# Title:       Important Security Announcement for python36 SUSE-SU-2020:3563-1
+# Description: Security fixes for SUSE Linux Enterprise 12 SP5
+# Source:      Security Announcement Parser v1.5.2
+# Modified:    2020 Dec 10
+#
+##############################################################################
+# Copyright (C) 2020 SUSE LLC
+##############################################################################
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+#  Authors/Contributors:
+#   Jason Record (jason.record@suse.com)
+#
+##############################################################################
+
+import os
+import Core
+import SUSE
+
+META_CLASS = "Security"
+META_CATEGORY = "SLE"
+META_COMPONENT = "python36"
+PATTERN_ID = os.path.basename(__file__)
+PRIMARY_LINK = "META_LINK_Security"
+OVERALL = Core.TEMP
+OVERALL_INFO = "NOT SET"
+OTHER_LINKS = "META_LINK_Security=https://lists.suse.com/pipermail/sle-security-updates/2020-November/007892.html"
+Core.init(META_CLASS, META_CATEGORY, META_COMPONENT, PATTERN_ID, PRIMARY_LINK, OVERALL, OVERALL_INFO, OTHER_LINKS)
+
+LTSS = False
+NAME = 'python36'
+MAIN = ''
+SEVERITY = 'Important'
+TAG = 'SUSE-SU-2020:3563-1'
+PACKAGES = {}
+SERVER = SUSE.getHostInfo()
+
+if ( SERVER['DistroVersion'] == 12):
+	if ( SERVER['DistroPatchLevel'] == 5 ):
+		PACKAGES = {
+			'libpython3_6m1_0': '3.6.12-4.22.2',
+			'libpython3_6m1_0-debuginfo': '3.6.12-4.22.2',
+			'python36': '3.6.12-4.22.2',
+			'python36-base': '3.6.12-4.22.2',
+			'python36-base-debuginfo': '3.6.12-4.22.2',
+			'python36-debuginfo': '3.6.12-4.22.2',
+			'python36-debugsource': '3.6.12-4.22.2',
+		}
+		SUSE.securityAnnouncementPackageCheck(NAME, MAIN, LTSS, SEVERITY, TAG, PACKAGES)
+	else:
+		Core.updateStatus(Core.ERROR, "ERROR: " + NAME + " Security Announcement: Outside the service pack scope")
+else:
+	Core.updateStatus(Core.ERROR, "ERROR: " + NAME + " Security Announcement: Outside the distribution scope")
+Core.printPatternResults()
+
