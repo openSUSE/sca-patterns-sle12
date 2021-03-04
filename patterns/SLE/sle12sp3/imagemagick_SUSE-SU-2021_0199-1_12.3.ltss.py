3945a0bc07d1aea556571bbf317245772d252905
Jason Record <jason.record@gmail.com>
2021-03-03 19:48:34 -0700

    Jan 2021 New Security Announcement Patterns 49

---
+++ patterns/SLE/sle12sp3/imagemagick_SUSE-SU-2021_0199-1_12.3.ltss.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+#
+# Title:       Important Security Announcement for ImageMagick SUSE-SU-2021:0199-1
+# Description: Security fixes for SUSE Linux Enterprise 12 SP3 LTSS
+# Source:      Security Announcement Parser v1.6.1
+# Modified:    2021 Mar 03
+#
+##############################################################################
+# Copyright (C) 2021 SUSE LLC
+##############################################################################
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+#  Authors/Contributors:
+#   Jason Record <jason.record@suse.com>
+#
+##############################################################################
+
+import os
+import Core
+import SUSE
+
+META_CLASS = "Security"
+META_CATEGORY = "SLE"
+META_COMPONENT = "ImageMagick"
+PATTERN_ID = os.path.basename(__file__)
+PRIMARY_LINK = "META_LINK_Security"
+OVERALL = Core.TEMP
+OVERALL_INFO = "NOT SET"
+OTHER_LINKS = "META_LINK_Security=https://lists.suse.com/pipermail/sle-security-updates/2021-January/008240.html"
+Core.init(META_CLASS, META_CATEGORY, META_COMPONENT, PATTERN_ID, PRIMARY_LINK, OVERALL, OVERALL_INFO, OTHER_LINKS)
+
+LTSS = True
+NAME = 'ImageMagick'
+MAIN = ''
+SEVERITY = 'Important'
+TAG = 'SUSE-SU-2021:0199-1'
+PACKAGES = {}
+SERVER = SUSE.getHostInfo()
+
+if ( SERVER['DistroVersion'] == 12):
+	if ( SERVER['DistroPatchLevel'] == 3 ):
+		PACKAGES = {
+			'ImageMagick-config-6-SUSE': '6.8.8.1-71.154.1',
+			'ImageMagick-config-6-upstream': '6.8.8.1-71.154.1',
+			'ImageMagick-debuginfo': '6.8.8.1-71.154.1',
+			'ImageMagick-debugsource': '6.8.8.1-71.154.1',
+			'libMagickCore-6_Q16-1': '6.8.8.1-71.154.1',
+			'libMagickCore-6_Q16-1-debuginfo': '6.8.8.1-71.154.1',
+			'libMagickWand-6_Q16-1': '6.8.8.1-71.154.1',
+			'libMagickWand-6_Q16-1-debuginfo': '6.8.8.1-71.154.1',
+		}
+		SUSE.securityAnnouncementPackageCheck(NAME, MAIN, LTSS, SEVERITY, TAG, PACKAGES)
+	else:
+		Core.updateStatus(Core.ERROR, "ERROR: " + NAME + " Security Announcement: Outside the service pack scope")
+else:
+	Core.updateStatus(Core.ERROR, "ERROR: " + NAME + " Security Announcement: Outside the distribution scope")
+Core.printPatternResults()
+
