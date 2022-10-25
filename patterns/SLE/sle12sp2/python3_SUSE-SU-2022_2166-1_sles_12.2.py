824ed5b1984afe236b470ba6266708878279a513
Jason Record <g23guy@gmail.com>
2022-10-25 08:16:42 -0600

    Jun 2022 New Security Announcement Patterns 176

---
+++ patterns/SLE/sle12sp2/python3_SUSE-SU-2022_2166-1_sles_12.2.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+#
+# Title:       Important Security Announcement for python3 SUSE-SU-2022:2166-1
+# Description: Security fixes for SUSE Linux Enterprise 12 SP2
+# URL:         https://lists.suse.com/pipermail/sle-security-updates/2022-June/011339.html
+# Source:      Security Announcement Generator (sagen.py) v2.0.0-beta4
+# Modified:    2022 Oct 05
+#
+##############################################################################
+# Copyright (C) 2022 SUSE LLC
+##############################################################################
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+#  Authors/Contributors:
+#   Jason Record <jason.record@suse.com>
+#
+##############################################################################
+
+import os
+import Core
+import SUSE
+
+META_CLASS = "Security"
+META_CATEGORY = "SLE"
+META_COMPONENT = "python3"
+pattern_filename = os.path.basename(__file__)
+PRIMARY_LINK = "META_LINK_Security"
+OVERALL = Core.TEMP
+OVERALL_INFO = "NOT SET"
+OTHER_LINKS = "META_LINK_Security=https://lists.suse.com/pipermail/sle-security-updates/2022-June/011339.html"
+Core.init(META_CLASS, META_CATEGORY, META_COMPONENT, pattern_filename, PRIMARY_LINK, OVERALL, OVERALL_INFO, OTHER_LINKS)
+
+LTSS = False
+NAME = 'python3'
+MAIN = ''
+SEVERITY = 'Important'
+TAG = 'SUSE-SU-2022:2166-1'
+PACKAGES = {}
+SERVER = SUSE.getHostInfo()
+
+if ( SERVER['DistroVersion'] == 12):
+	if ( SERVER['DistroPatchLevel'] == 2 ):
+		PACKAGES = {
+			'libpython3_4m1_0': '3.4.10-25.93.1',
+			'libpython3_4m1_0-debuginfo': '3.4.10-25.93.1',
+			'python3': '3.4.10-25.93.1',
+			'python3-base': '3.4.10-25.93.1',
+			'python3-base-debuginfo': '3.4.10-25.93.1',
+			'python3-base-debugsource': '3.4.10-25.93.1',
+			'python3-curses': '3.4.10-25.93.1',
+			'python3-curses-debuginfo': '3.4.10-25.93.1',
+			'python3-debuginfo': '3.4.10-25.93.1',
+			'python3-debugsource': '3.4.10-25.93.1',
+		}
+		SUSE.securityAnnouncementPackageCheck(NAME, MAIN, LTSS, SEVERITY, TAG, PACKAGES)
+	else:
+		Core.updateStatus(Core.ERROR, "ERROR: " + NAME + " Security Announcement: Outside the service pack scope")
+else:
+	Core.updateStatus(Core.ERROR, "ERROR: " + NAME + " Security Announcement: Outside the distribution scope")
+Core.printPatternResults()
+
---
+++ patterns/SLE/sle12sp4/mozilla-nss_SUSE-SU-2022_2031-1_sles_12.4.ltss.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+#
+# Title:       Important Security Announcement for mozilla-nss SUSE-SU-2022:2031-1
+# Description: Security fixes for SUSE Linux Enterprise 12 SP4 LTSS
+# URL:         https://lists.suse.com/pipermail/sle-security-updates/2022-June/011255.html
+# Source:      Security Announcement Generator (sagen.py) v2.0.0-beta4
+# Modified:    2022 Oct 05
+#
+##############################################################################
+# Copyright (C) 2022 SUSE LLC
+##############################################################################
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+#  Authors/Contributors:
+#   Jason Record <jason.record@suse.com>
+#
+##############################################################################
+
+import os
+import Core
+import SUSE
+
+META_CLASS = "Security"
+META_CATEGORY = "SLE"
+META_COMPONENT = "mozilla-nss"
+pattern_filename = os.path.basename(__file__)
+PRIMARY_LINK = "META_LINK_Security"
+OVERALL = Core.TEMP
+OVERALL_INFO = "NOT SET"
+OTHER_LINKS = "META_LINK_Security=https://lists.suse.com/pipermail/sle-security-updates/2022-June/011255.html"
+Core.init(META_CLASS, META_CATEGORY, META_COMPONENT, pattern_filename, PRIMARY_LINK, OVERALL, OVERALL_INFO, OTHER_LINKS)
+
+LTSS = True
+NAME = 'mozilla-nss'
+MAIN = ''
+SEVERITY = 'Important'
+TAG = 'SUSE-SU-2022:2031-1'
+PACKAGES = {}
+SERVER = SUSE.getHostInfo()
+
+if ( SERVER['DistroVersion'] == 12):
+	if ( SERVER['DistroPatchLevel'] == 4 ):
+		PACKAGES = {
+			'libfreebl3': '3.68.4-58.72.1',
+			'libfreebl3-debuginfo': '3.68.4-58.72.1',
+			'libfreebl3-hmac': '3.68.4-58.72.1',
+			'libsoftokn3': '3.68.4-58.72.1',
+			'libsoftokn3-debuginfo': '3.68.4-58.72.1',
+			'libsoftokn3-hmac': '3.68.4-58.72.1',
+			'mozilla-nss': '3.68.4-58.72.1',
+			'mozilla-nss-certs': '3.68.4-58.72.1',
+			'mozilla-nss-certs-debuginfo': '3.68.4-58.72.1',
+			'mozilla-nss-debuginfo': '3.68.4-58.72.1',
+			'mozilla-nss-debugsource': '3.68.4-58.72.1',
+			'mozilla-nss-devel': '3.68.4-58.72.1',
+			'mozilla-nss-sysinit': '3.68.4-58.72.1',
+			'mozilla-nss-sysinit-debuginfo': '3.68.4-58.72.1',
+			'mozilla-nss-tools': '3.68.4-58.72.1',
+			'mozilla-nss-tools-debuginfo': '3.68.4-58.72.1',
+		}
+		SUSE.securityAnnouncementPackageCheck(NAME, MAIN, LTSS, SEVERITY, TAG, PACKAGES)
+	else:
+		Core.updateStatus(Core.ERROR, "ERROR: " + NAME + " Security Announcement: Outside the service pack scope")
+else:
+	Core.updateStatus(Core.ERROR, "ERROR: " + NAME + " Security Announcement: Outside the distribution scope")
+Core.printPatternResults()
+
